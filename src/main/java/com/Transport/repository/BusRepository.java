5ea8264b3cc30dc0b46ad227b8c4af20cc7bd9a7
Satrujit Biswal <149881810+SatrujitBiswal@users.noreply.github.com>
2025-09-20 20:33:58 +0530

    Update BusRepository.java
    check for pull

--- src/main/java/com/Transport/repository/BusRepository.java
+++ src/main/java/com/Transport/repository/BusRepository.java
@@ -3,7 +3,7 @@ package com.Transport.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Transport.entity.Bus;
-
+// pull
 public interface BusRepository extends JpaRepository<Bus, Long>{
 
 }
